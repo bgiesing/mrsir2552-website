5d2a9c12a580fe71dccfadae89d779eb33e30214
Brandon Giesing <me@brandongiesing.com>
2020-01-16 16:51:58 -0600

    Update Stats.jsx

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -27,7 +27,7 @@ class Stats extends Component {
         </StatItem>
         <StatItem {...this.props}>
           <FontAwesomeIcon icon={faUserFriends} style={{ marginRight: '5px' }} />
-          {this.props.followers.followers.total && this.props.followers.followers.total.toLocaleString()}
+          {this.props.user.followers.total && this.props.user.followers.total.toLocaleString()}
         </StatItem>
       </StatsDiv>
     );
